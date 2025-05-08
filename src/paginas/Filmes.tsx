903f37daa0600afbef43ea3b50d8caa3e2fe2472
Gabriel Marcos Lopes <gabriel.lopes3@estudante.ufla.br>
2025-05-08 17:45:52 -0300

    style: eslizando lista de filmes na pagina principal

--- src/paginas/Filmes.tsx
+++ src/paginas/Filmes.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import api from '../servicos/api';
 import { Link } from 'react-router-dom';
+import '../estilos/filmes.scss';
 
 interface Filme {
   id: number;
@@ -26,24 +27,26 @@ function Filmes() {
 
   return (
     <div className="lista">
-      <h1 className="titulo">Filmes Mais Populares</h1>
-      {filmes.length === 0 ? (
-        <p>Carregando filmes...</p>
-      ) : (
-      filmes.map(filme => (
-        <div className="coluna" key={filme.id}>
-          <Link to={`/movie/${filme.id}`}>
-            <img
-              src={`https://image.tmdb.org/t/p/w500${filme.poster_path}`}
-              className="imagem-filme"
-              alt={filme.title}
-            />
-            <h2 className="titulo">{filme.title}</h2>
-            <p className="avaliacao">  {filme.vote_average}</p>
-          </Link>
-        </div>
-        ))
-      )}
+      <h1 className="titulo-pagina">Filmes Mais Populares</h1>
+      <div className='grade'>
+        {filmes.length === 0 ? (
+          <p>Carregando filmes...</p>
+        ) : (
+        filmes.map(filme => (
+          <div className="card" key={filme.id}>
+            <Link to={`/movie/${filme.id}`}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${filme.poster_path}`}
+                className="imagem-filme"
+                alt={filme.title}
+              />
+              <h2 className="titulo">{filme.title}</h2>
+              <p className="avaliacao">⭐{filme.vote_average.toFixed(1)}</p>
+            </Link>
+          </div>
+          ))
+        )}
+      </div>
     </div>
   );
 }
