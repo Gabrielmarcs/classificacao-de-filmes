17ec2748e4dabfee3f199982646224ef1a141a8e
Gabriel Marcos Lopes <gabriel.lopes3@estudante.ufla.br>
2025-05-08 12:35:49 -0300

    chore: adicionando fonte na página

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './estilos/global.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
